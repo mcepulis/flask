9fe104bf26d125cd2c5b7f57f511c6045a40768a
mcepulis <109752570+mcepulis@users.noreply.github.com>
2025-01-23 16:04:49 +0200

    completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,31 @@
-import React, {useState, useEffect} from 'react';
+import {useState, useEffect} from 'react';
 
 function App() {
   const [data, setData] = useState({})
 
   useEffect(() => {
-    fetch("/members").then(
-      res=>res.json()
-    ).then(data=>{
-      setData(data)
-      console.log(data)
-    })
-})
+    fetch("http://localhost:5000/members")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []); 
+
+  
   return (
-  <div>
-      {(typeof data.members === 'undefined') ? (
-        <p>loading...</p>
+    <div>
+      {!data.members ? (
+        <p>Loading...</p> 
       ) : (
         data.members.map((member, i) => (
           <p key={i}>{member}</p>
-      ))
+        ))
       )}
-  </div>
-  )
+    </div>
+  );
 }
 export default App;
